32b04aae49d5b7c11baa6a713f0ae997f8a4aa89
Nathália Pissuti <nathaliapissuti@gmail.com>
2025-08-24 10:29:00 -0300

    style: Improve workout layout for small devices

--- src/components/Forms/Select/Select.jsx
+++ src/components/Forms/Select/Select.jsx
@@ -1,13 +1,19 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import styles from './Select.module.scss';
+
 const Select = (props) => {
   const { renderItems = () => null, emptyItemText = 'Select item', value, ...otherProps } = props;
 
   const { t } = useTranslation();
 
   return (
-    <select value={value || ''} {...otherProps}>
+    <select
+      value={value || ''}
+      className={styles.Select}
+      {...otherProps}
+    >
       <option value="">{t(emptyItemText)}</option>
       {renderItems()}
     </select>
